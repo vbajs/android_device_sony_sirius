2abae4dc3e44bc2b2a4047037ea7acc4e636f80c
Yahya Wessam <vbajs@users.noreply.github.com>
2016-05-04 20:29:45 +0300

    Turbo-ify

--- cm.mk
+++ cm.mk
@@ -1,8 +1,8 @@
-# Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit Turbo common Phone stuff.
+$(call inherit-product, vendor/turbo/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/turbo/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/sony/sirius/full_sirius.mk)
 
@@ -10,5 +10,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D6503
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D6503/D6503:5.1.1/23.4.A.0.546/864948651:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D6503-user 5.1.1 23.4.A.0.546 864948651 release-keys"
 
-PRODUCT_NAME := cm_sirius
+PRODUCT_NAME := turbo_sirius
 PRODUCT_DEVICE := sirius
